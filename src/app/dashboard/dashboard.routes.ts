4840413f9513da3aff2e5e4dcaec2200b9f5d134
Juan Manuel Galindo Casillas <casillasmanuel9@gmail.com>
2020-08-17 11:11:44 -0500

    diseno de aplicacion de ingresos y egresos

---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,19 @@
+import { DetalleComponent } from './../ingreso-egreso/detalle/detalle.component';
+import { IngresoEgresoComponent } from './../ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from './../ingreso-egreso/estadistica/estadistica.component';
+import { Routes } from '@angular/router';
+
+export const dashBoardRoutes : Routes = [
+  {
+    path: '',
+    component: EstadisticaComponent
+  },
+  {
+    path: 'ingreso-egreso',
+    component: IngresoEgresoComponent
+  },
+  {
+    path: 'detalle',
+    component: DetalleComponent
+  }
+]
